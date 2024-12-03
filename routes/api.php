5999738502c00b3c79c8c9e715cda7f92564273a
tommypatra <tommyirawan.patra@gmail.com>
2024-12-03 17:17:52 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -81,10 +81,10 @@
     });
 
 
-    Route::fallback(function () {
-        return response()->json([
-            'status' => false,
-            'message' => 'Route not found',
-            'data' => null,
-        ], 404);
-    });
+    // Route::fallback(function () {
+    //     return response()->json([
+    //         'status' => false,
+    //         'message' => 'Route not found',
+    //         'data' => null,
+    //     ], 404);
+    // });
